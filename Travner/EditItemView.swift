ba4d08c2bc19effa340cfbfd32b42f143c63b3e2
Lorenzo Lins Mazzarotto <mazzarottolorenzo@icloud.com>
2022-04-28 00:19:16 +0200

    The problem with onDisappear()

--- Travner/EditItemView.swift
+++ Travner/EditItemView.swift
@@ -47,7 +47,10 @@ struct EditItemView: View {
             }
         }
         .navigationTitle("Edit Item")
-        .onDisappear(perform: update)
+        .onChange(of: title) { _ in update() }
+        .onChange(of: detail) { _ in update() }
+        .onChange(of: priority) { _ in update() }
+        .onChange(of: completed) { _ in update() }
     }
 
     func update() {
