8c475817f4c15379b85db29f8e75ff9270dc31a5
Lorenzo Lins Mazzarotto <mazzarottolorenzo@icloud.com>
2022-04-27 22:55:11 +0200

    Creating custom extensions

---
+++ Travner/Item-CoreDataHelpers.swift
@@ -0,0 +1,34 @@
+//
+//  Item-CoreDataHelpers.swift
+//  Travner
+//
+//  Created by Lorenzo Lins Mazzarotto on 27/04/22.
+//
+
+import Foundation
+
+extension Item {
+    var itemTitle: String {
+        title ?? ""
+    }
+
+    var itemDetail: String {
+        detail ?? ""
+    }
+
+    var itemCreationDate: Date {
+        creationDate ?? Date()
+    }
+
+    static var example: Item {
+        let controller = DataController(inMemory: true)
+        let viewContext = controller.container.viewContext
+
+        let item = Item(context: viewContext)
+        item.title = "Example Item"
+        item.detail = "This is an example item"
+        item.priority = 3
+        item.creationDate = Date()
+        return item
+    }
+}
