0530f22ddc3b60e8583a4b9a32da0034da00e16a
Lorenzo Lins Mazzarotto <mazzarottolorenzo@icloud.com>
2022-04-29 08:02:37 +0200

    Which to choose?

--- Travner/ProjectsView.swift
+++ Travner/ProjectsView.swift
@@ -33,7 +33,7 @@ struct ProjectsView: View {
             List {
                 ForEach(projects.wrappedValue) { project in
                     Section(header: ProjectHeaderView(project: project)) {
-                        ForEach(items(for: project)) { item in
+                        ForEach(project.projectItems(using: sortOrder)) { item in
                             ItemRowView(item: item)
                         }
                         .onDelete { offsets in
@@ -95,17 +95,6 @@ struct ProjectsView: View {
                     .default(Text("Title")) { sortOrder = .title }
                 ])
             }
-        }
-    }
-
-    func items(for project: Project) -> [Item] {
-        switch sortOrder {
-        case .title:
-            return project.projectItems.sorted { $0.itemTitle < $1.itemTitle }
-        case .creationDate:
-            return project.projectItems.sorted { $0.itemCreationDate < $1.itemCreationDate }
-        case .optimized:
-            return project.projectItemsDefaultSorted
         }
     }
 }
