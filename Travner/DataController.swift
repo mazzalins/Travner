f3baa4d7caf30f078b1d76f897863784efff38cb
Lorenzo Lins Mazzarotto <mazzarottolorenzo@icloud.com>
2022-04-30 19:27:08 +0200

    Explaining our code

--- Travner/DataController.swift
+++ Travner/DataController.swift
@@ -14,6 +14,9 @@ class DataController: ObservableObject {
     init(inMemory: Bool = false) {
         container = NSPersistentCloudKitContainer(name: "Main")
 
+        // For testing and previewing purposes, we create a
+        // temporary, in-memory database by writing to /dev/null
+        // so our data is destroyed after the app finishes running.
         if inMemory {
             container.persistentStoreDescriptions.first?.url = URL(fileURLWithPath: "/dev/null")
         }
@@ -88,17 +91,20 @@ class DataController: ObservableObject {
     func hasEarned(award: Award) -> Bool {
         switch award.criterion {
         case "items":
+            // returns true if they added a certain number of items
             let fetchRequest: NSFetchRequest<Item> = NSFetchRequest(entityName: "Item")
             let awardCount = count(for: fetchRequest)
             return awardCount >= award.value
 
         case "complete":
+            // returns true if they completed a certain number of items
             let fetchRequest: NSFetchRequest<Item> = NSFetchRequest(entityName: "Item")
             fetchRequest.predicate = NSPredicate(format: "completed = true")
             let awardCount = count(for: fetchRequest)
             return awardCount >= award.value
 
         default:
+            // an unknown award criterion; this should never be allowed
             // fatalError("Unknown award criterion \(award.criterion).")
             return false
         }
