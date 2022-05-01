5ecc76a4b7d6e81db4dfe4d697d7d3b92dad40e5
Lorenzo Lins Mazzarotto <mazzarottolorenzo@icloud.com>
2022-05-01 23:11:37 +0200

    Verifying the empty state

--- TravnerUITests/TravnerUITests.swift
+++ TravnerUITests/TravnerUITests.swift
@@ -8,14 +8,17 @@
 import XCTest
 
 class TravnerUITests: XCTestCase {
+    var app: XCUIApplication!
 
     override func setUpWithError() throws {
         continueAfterFailure = false
-    }
 
-    func testExample() throws {
-        let app = XCUIApplication()
+        app = XCUIApplication()
         app.launchArguments = ["enable-testing"]
         app.launch()
+    }
+
+    func testAppHas4Tabs() throws {
+        XCTAssertEqual(app.tabBars.buttons.count, 4, "There should be 4 tabs in the app.")
     }
 }
