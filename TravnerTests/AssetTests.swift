5e3c5b9823764dd36c17058c1edeb71ef8e76dcc
Lorenzo Lins Mazzarotto <mazzarottolorenzo@icloud.com>
2022-05-01 12:40:40 +0200

    Testing bundled data

---
+++ TravnerTests/AssetTests.swift
@@ -0,0 +1,21 @@
+//
+//  AssetTests.swift
+//  TravnerTests
+//
+//  Created by Lorenzo Lins Mazzarotto on 01/05/22.
+//
+
+import XCTest
+@testable import Travner
+
+class AssetTests: XCTestCase {
+    func testColorsExist() {
+        for color in Project.colors {
+            XCTAssertNotNil(UIColor(named: color), "Failed to load color '\(color)' from asset catalog.")
+        }
+    }
+
+    func testJSONLoadsCorrectly() {
+        XCTAssertTrue(Award.allAwards.isEmpty == false, "Failed to load awards from JSON.")
+    }
+}
