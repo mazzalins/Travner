b2b53f417326a72131accb4b95e82c5d983349c5
Lorenzo Lins Mazzarotto <mazzarottolorenzo@icloud.com>
2022-05-01 21:57:15 +0200

    Quirks of Core Data

--- TravnerTests/DevelopmentTests.swift
+++ TravnerTests/DevelopmentTests.swift
@@ -24,4 +24,14 @@ class DevelopmentTests: BaseTestCase {
         XCTAssertEqual(dataController.count(for: Project.fetchRequest()), 0, "deleteAll() should leave 0 projects.")
         XCTAssertEqual(dataController.count(for: Item.fetchRequest()), 0, "deleteAll() should leave 0 items.")
     }
+
+    func testExampleProjectIsClosed() {
+        let project = Project.example
+        XCTAssertTrue(project.closed, "The example project should be closed.")
+    }
+
+    func testExampleItemIsHighPriority() {
+        let item = Item.example
+        XCTAssertEqual(item.priority, 3, "The example item should be high priority.")
+    }
 }
