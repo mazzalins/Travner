a69ba8154bbc0f15a43fb661f8f59806f1f79ac5
Lorenzo Lins Mazzarotto <mazzarottolorenzo@icloud.com>
2022-05-01 23:35:03 +0200

    Examining detail views

--- TravnerUITests/TravnerUITests.swift
+++ TravnerUITests/TravnerUITests.swift
@@ -42,4 +42,44 @@ class TravnerUITests: XCTestCase {
         app.buttons["Add New Item"].tap()
         XCTAssertEqual(app.tables.cells.count, 2, "There should be 2 list rows after adding an item.")
     }
+
+    func testEditingProjectUpdatesCorrectly() {
+        app.buttons["Open"].tap()
+        XCTAssertEqual(app.tables.cells.count, 0, "There should be no list rows initially.")
+
+        app.buttons["Add Project"].tap()
+        XCTAssertEqual(app.tables.cells.count, 1, "There should be 1 list row after adding a project.")
+
+        app.buttons["Compose"].tap()
+        app.textFields["Project name"].tap()
+
+        app.keys["space"].tap()
+        app.keys["more"].tap()
+        app.keys["2"].tap()
+        app.buttons["Return"].tap()
+
+        app.buttons["Open Projects"].tap()
+
+        XCTAssertTrue(
+            app.tables.staticTexts["New Project 2"].exists,
+            "The new project name should be visible in the list."
+        )
+    }
+
+    func testEditingItemUpdatesCorrectly() {
+        // Go to Open Projects and add one project and one item.
+        testAddingItemInsertsRows()
+
+        app.buttons["New Item"].tap()
+        app.textFields["Item name"].tap()
+
+        app.keys["space"].tap()
+        app.keys["more"].tap()
+        app.keys["2"].tap()
+        app.buttons["Return"].tap()
+
+        app.buttons["Open Projects"].tap()
+
+        XCTAssertTrue(app.buttons["New Item 2"].exists, "The new item name should be visible in the list.")
+    }
 }
