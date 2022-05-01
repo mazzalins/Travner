5c2fb5f99d8eea1cff4af0357bb0d60f22becdb1
Lorenzo Lins Mazzarotto <mazzarottolorenzo@icloud.com>
2022-05-01 23:01:47 +0200

    Verifying the work

--- TravnerTests/PerformanceTests.swift
+++ TravnerTests/PerformanceTests.swift
@@ -17,9 +17,10 @@ class PerformanceTests: BaseTestCase {
 
         // Simulate lots of awards to check
         let awards = Array(repeating: Award.allAwards, count: 25).joined()
+        XCTAssertEqual(awards.count, 500, "This checks the awards count is constant. Change this if you add awards.")
 
         measure {
-            _ = awards.filter(dataController.hasEarned)
+            _ = awards.filter(dataController.hasEarned).count
         }
     }
 }
