d4894252acec76e4b9f933ee606c4c2752b47d98
Lorenzo Lins Mazzarotto <mazzarottolorenzo@icloud.com>
2022-05-01 21:49:54 +0200

    Dealing with entities

--- TravnerTests/DevelopmentTests.swift
+++ TravnerTests/DevelopmentTests.swift
@@ -16,4 +16,12 @@ class DevelopmentTests: BaseTestCase {
         XCTAssertEqual(dataController.count(for: Project.fetchRequest()), 5, "There should be 5 sample projects.")
         XCTAssertEqual(dataController.count(for: Item.fetchRequest()), 50, "There should be 50 sample items.")
     }
+
+    func testDeleteAllClearsEverything() throws {
+        try dataController.createSampleData()
+        dataController.deleteAll()
+
+        XCTAssertEqual(dataController.count(for: Project.fetchRequest()), 0, "deleteAll() should leave 0 projects.")
+        XCTAssertEqual(dataController.count(for: Item.fetchRequest()), 0, "deleteAll() should leave 0 items.")
+    }
 }
