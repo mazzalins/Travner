cc364cfee39ee1bfc8ef7c388a268d98b4410158
Lorenzo Lins Mazzarotto <mazzarottolorenzo@icloud.com>
2022-05-01 12:33:58 +0200

    Getting ready for testing

---
+++ TravnerTests/TravnerTests.swift
@@ -0,0 +1,21 @@
+//
+//  TravnerTests.swift
+//  TravnerTests
+//
+//  Created by Lorenzo Lins Mazzarotto on 01/05/22.
+//
+
+import CoreData
+import XCTest
+@testable import Travner
+
+class BaseTestCase: XCTestCase {
+
+    var dataController: DataController!
+    var managedObjectContext: NSManagedObjectContext!
+
+    override func setUpWithError() throws {
+        dataController = DataController(inMemory: true)
+        managedObjectContext = dataController.container.viewContext
+    }
+}
