a2a832b017c7779cbbe881921e803dca63fef2b2
Lorenzo Lins Mazzarotto <mazzarottolorenzo@icloud.com>
2022-05-03 00:18:50 +0200

    Advanced haptics

--- Travner/Activities/Projects/EditProjectView.swift
+++ Travner/Activities/Projects/EditProjectView.swift
@@ -5,6 +5,7 @@
 //  Created by Lorenzo Lins Mazzarotto on 28/04/22.
 //
 
+import CoreHaptics
 import SwiftUI
 
 struct EditProjectView: View {
@@ -17,6 +18,8 @@ struct EditProjectView: View {
     @State private var detail: String
     @State private var color: String
     @State private var showingDeleteConfirm = false
+
+    @State private var engine = try? CHHapticEngine()
 
     let colorColumns = [
         GridItem(.adaptive(minimum: 44))
@@ -46,13 +49,7 @@ struct EditProjectView: View {
 
             // swiftlint:disable:next line_length
             Section(footer: Text("Closing a project moves it from the Open to Closed tab; deleting it removes the project completely.")) {
-                Button(project.closed ? "Reopen this project" : "Close this project") {
-                    project.closed.toggle()
-
-                    if project.closed {
-                        UINotificationFeedbackGenerator().notificationOccurred(.success)
-                    }
-                }
+                Button(project.closed ? "Reopen this project" : "Close this project", action: toggleClosed)
 
                 Button("Delete this project") {
                     showingDeleteConfirm.toggle()
@@ -81,6 +78,50 @@ struct EditProjectView: View {
     func delete() {
         dataController.delete(project)
         presentationMode.wrappedValue.dismiss()
+    }
+
+    func toggleClosed() {
+        project.closed.toggle()
+
+        if project.closed {
+            do {
+                try engine?.start()
+
+                let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 0)
+                let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: 1)
+
+                let start = CHHapticParameterCurve.ControlPoint(relativeTime: 0, value: 1)
+                let end = CHHapticParameterCurve.ControlPoint(relativeTime: 1, value: 0)
+
+                // use that curve to control the haptic strength
+                let parameter = CHHapticParameterCurve(
+                    parameterID: .hapticIntensityControl,
+                    controlPoints: [start, end],
+                    relativeTime: 0
+                )
+
+                let event1 = CHHapticEvent(
+                    eventType: .hapticTransient,
+                    parameters: [intensity, sharpness],
+                    relativeTime: 0
+                )
+
+                // create a continuous haptic event starting immediately and lasting one second
+                let event2 = CHHapticEvent(
+                    eventType: .hapticContinuous,
+                    parameters: [sharpness, intensity],
+                    relativeTime: 0.125,
+                    duration: 1
+                )
+
+                let pattern = try CHHapticPattern(events: [event1, event2], parameterCurves: [parameter])
+
+                let player = try engine?.makePlayer(with: pattern)
+                try player?.start(atTime: 0)
+            } catch {
+                // playing haptics didn't work, but that's okay
+            }
+        }
     }
 
     func colorButton(for item: String) -> some View {
