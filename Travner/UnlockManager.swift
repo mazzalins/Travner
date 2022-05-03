034ca0d79afde5fddcaa6c01c2eb252886e856c1
Lorenzo Lins Mazzarotto <mazzarottolorenzo@icloud.com>
2022-05-03 19:12:42 +0200

    Handling a purchase

--- Travner/UnlockManager.swift
+++ Travner/UnlockManager.swift
@@ -11,10 +11,14 @@ import StoreKit
 class UnlockManager: NSObject, ObservableObject, SKPaymentTransactionObserver, SKProductsRequestDelegate {
     enum RequestState {
         case loading
-        case loaded
-        case failed
+        case loaded(SKProduct)
+        case failed(Error?)
         case purchased
         case deferred
+    }
+
+    private enum StoreError: Error {
+        case invalidIdentifiers, missingProduct
     }
 
     @Published var requestState = RequestState.loading
@@ -22,6 +26,10 @@ class UnlockManager: NSObject, ObservableObject, SKPaymentTransactionObserver, S
     private let dataController: DataController
     private let request: SKProductsRequest
     private var loadedProducts = [SKProduct]()
+
+    var canMakePayments: Bool {
+        SKPaymentQueue.canMakePayments()
+    }
 
     init(dataController: DataController) {
         // Store the data controller we were sent.
@@ -37,6 +45,9 @@ class UnlockManager: NSObject, ObservableObject, SKPaymentTransactionObserver, S
         // Start watching the payment queue.
         SKPaymentQueue.default().add(self)
 
+        // Verify if the full version is not unlocked before asking for more products.
+        guard dataController.fullVersionUnlocked == false else { return }
+
         // Set ourselves up to be notified when the product request completes.
         request.delegate = self
 
@@ -49,10 +60,59 @@ class UnlockManager: NSObject, ObservableObject, SKPaymentTransactionObserver, S
     }
 
     func paymentQueue(_ queue: SKPaymentQueue, updatedTransactions transactions: [SKPaymentTransaction]) {
+        DispatchQueue.main.async { [self] in
+            for transaction in transactions {
+                switch transaction.transactionState {
+                case .purchased, .restored:
+                    self.dataController.fullVersionUnlocked = true
+                    self.requestState = .purchased
+                    queue.finishTransaction(transaction)
 
+                case .failed:
+                    if let product = loadedProducts.first {
+                        self.requestState = .loaded(product)
+                    } else {
+                        self.requestState = .failed(transaction.error)
+                    }
+
+                    queue.finishTransaction(transaction)
+
+                case .deferred:
+                    self.requestState = .deferred
+
+                default:
+                    break
+                }
+            }
+        }
     }
 
     func productsRequest(_ request: SKProductsRequest, didReceive response: SKProductsResponse) {
+        DispatchQueue.main.async {
+            // Store the returned products for later, if we need them.
+            self.loadedProducts = response.products
 
+            guard let unlock = self.loadedProducts.first else {
+                self.requestState = .failed(StoreError.missingProduct)
+                return
+            }
+
+            if response.invalidProductIdentifiers.isEmpty == false {
+                print("ALERT: Received invalid product identifiers: \(response.invalidProductIdentifiers)")
+                self.requestState = .failed(StoreError.invalidIdentifiers)
+                return
+            }
+
+            self.requestState = .loaded(unlock)
+        }
+    }
+
+    func buy(product: SKProduct) {
+        let payment = SKPayment(product: product)
+        SKPaymentQueue.default().add(payment)
+    }
+
+    func restore() {
+        SKPaymentQueue.default().restoreCompletedTransactions()
     }
 }
