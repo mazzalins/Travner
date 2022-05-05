383b91b40cf654362ca00460586d1e1af0aadf4e
Lorenzo Lins Mazzarotto <mazzarottolorenzo@icloud.com>
2022-05-05 03:14:11 +0200

    Inside widgets

--- Travner/DataController-StoreKit.swift
+++ Travner/DataController-StoreKit.swift
@@ -11,10 +11,10 @@ import StoreKit
 extension DataController {
     func appLaunched() {
         guard count(for: Project.fetchRequest()) >= 5 else { return }
-        
+
         let allScenes = UIApplication.shared.connectedScenes
         let scene = allScenes.first { $0.activationState == .foregroundActive }
-        
+
         if let windowScene = scene as? UIWindowScene {
             SKStoreReviewController.requestReview(in: windowScene)
         }
