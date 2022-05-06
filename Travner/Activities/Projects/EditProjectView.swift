04be457d5398b440b979bd925af04e12d008c479
Lorenzo Lins Mazzarotto <mazzarottolorenzo@icloud.com>
2022-05-06 03:14:21 +0200

    Communicating with iCloud

--- Travner/Activities/Projects/EditProjectView.swift
+++ Travner/Activities/Projects/EditProjectView.swift
@@ -5,6 +5,7 @@
 //  Created by Lorenzo Lins Mazzarotto on 28/04/22.
 //
 
+import CloudKit
 import CoreHaptics
 import SwiftUI
 
@@ -91,6 +92,23 @@ struct EditProjectView: View {
             }
         }
         .navigationTitle("Edit Project")
+        .toolbar {
+            Button {
+                let records = project.prepareCloudRecords()
+                let operation = CKModifyRecordsOperation(recordsToSave: records, recordIDsToDelete: nil)
+                operation.savePolicy = .allKeys
+
+                operation.modifyRecordsCompletionBlock = { _, _, error in
+                    if let error = error {
+                        print("Error: \(error.localizedDescription)")
+                    }
+                }
+
+                CKContainer.default().publicCloudDatabase.add(operation)
+            } label: {
+                Label("Upload to iCloud", systemImage: "icloud.and.arrow.up")
+            }
+        }
         .onDisappear(perform: dataController.save)
         .alert(isPresented: $showingDeleteConfirm) {
             Alert(
