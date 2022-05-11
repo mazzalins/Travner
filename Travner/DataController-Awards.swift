ef166289114abf193408a6f187c703ceb2c2c811
Lorenzo Lins Mazzarotto <mazzarottolorenzo@icloud.com>
2022-05-12 01:28:09 +0200

    Add "Supporter" award verification case

--- Travner/DataController-Awards.swift
+++ Travner/DataController-Awards.swift
@@ -23,10 +23,14 @@ extension DataController {
             fetchRequest.predicate = NSPredicate(format: "completed = true")
             let awardCount = count(for: fetchRequest)
             return awardCount >= award.value
-            
+
         case "chat":
             // returns true if they posted a certain number of chat messages
             return UserDefaults.standard.integer(forKey: "chatCount") >= award.value
+
+        case "unlock":
+            // returns true if they bought the premium unlock
+            return fullVersionUnlocked
 
         default:
             // an unknown award criterion; this should never be allowed
